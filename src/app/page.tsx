35ee0cee0644bbe1afa43ebc885cf6af46d289bd
valentyn <osiptsev.valik@gmail.com>
2023-10-26 01:54:49 +0300

    created & config button comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Button from '@/components/ui/Button/Button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="text-red-500">
+    <main>
       LALALLALAL
+      <Button>Click me</Button>
     </main>
   )
 }
