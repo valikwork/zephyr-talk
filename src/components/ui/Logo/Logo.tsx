9d5cd1992073feba98af58795246ddfc7b2b7260
valentyn <osiptsev.valik@gmail.com>
2023-10-27 14:49:54 +0300

    added google OAuth

---
+++ src/components/ui/Logo/Logo.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image';
+
+type LogoProps = {
+  width?: number,
+  height?: number,
+
+}
+
+const Logo = ({
+  width = 48,
+  height = 48
+}: LogoProps) => {
+  return (
+    <Image src='/images/zephyrLogo.webp' alt="ZephyrTalk logo" width={width} height={height} />
+  )
+}
+
+export default Logo
